7eda47dec5e0dcbbc4bb9b574b3a4ba1eecec00d
danieldeichfuss <d.deichfuss@gmail.com>
2021-10-24 12:55:42 +0200

    Fix linter

--- src/get-status/get-status.ts
+++ src/get-status/get-status.ts
@@ -1,6 +1,8 @@
 import {ActionInput, ActionOutput} from '../types'
 
 export function getStatus({ref, token}: ActionInput): ActionOutput {
+  console.log({ref, token})
+
   return {
     allChecksCompleted: true,
     allChecksPassed: true
