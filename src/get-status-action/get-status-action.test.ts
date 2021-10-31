ddce425f41ffb927f079f0deb403d9e45b35d636
Daniel Deichfuß <danieldeichfuss@users.noreply.github.com>
2021-10-31 15:01:05 +0100

    Add tests for get status action (#22)
    * Test getStatus call
    
    * Add Tests for getStatusAction

--- src/get-status-action/get-status-action.test.ts
+++ src/get-status-action/get-status-action.test.ts
@@ -1,10 +1,57 @@
 import {getStatusAction} from './get-status-action'
-import {it, expect, jest} from '@jest/globals'
+import {it, expect, jest, beforeEach} from '@jest/globals'
+import {getStatus} from '../get-status'
+import * as core from '@actions/core'
 
-it('should get the inputs for the action', () => {})
+jest.mock('../get-status')
 
-it('should call getStatus with the inputs', () => {})
+jest.mock('@actions/core', () => {
+  return {
+    getInput: jest.fn().mockImplementation(input => input),
+    setOutput: jest.fn(),
+    setFailed: jest.fn()
+  }
+})
 
-it('should set the action output', () => {})
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
-it('should set the action to failed', () => {})
+it('should get the inputs for the action', () => {
+  getStatusAction()
+
+  expect(core.getInput).toHaveBeenCalledWith('ref')
+  expect(core.getInput).toHaveBeenCalledWith('token')
+})
+
+it('should call getStatus with the inputs', () => {
+  getStatusAction()
+
+  expect(getStatus).toHaveBeenCalledWith({
+    ref: 'ref',
+    token: 'token'
+  })
+})
+
+it('should set the action output', () => {
+  ;(getStatus as any).mockReturnValue({
+    allChecksCompleted: true,
+    allChecksPassed: true
+  })
+
+  getStatusAction()
+
+  expect(core.setOutput).toHaveBeenCalledWith('all-checks-completed', true)
+  expect(core.setOutput).toHaveBeenCalledWith('all-checks-passed', true)
+})
+
+it('should set the action to failed', () => {
+  const errorMessage = 'test error'
+  ;(getStatus as any).mockImplementation(() => {
+    throw new Error(errorMessage)
+  })
+
+  getStatusAction()
+
+  expect(core.setFailed).toHaveBeenCalledWith(errorMessage)
+})
