b8f9a10e94baeec11f80f7f9870cb385750fbcc1
danieldeichfuss <d.deichfuss@gmail.com>
2021-11-02 12:17:13 +0100

    Add check-suites mock handler

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -1,14 +1,9 @@
 const {rest} = require('msw')
-const allChecksPassed = require('./__fixtures__/all-checks-passed.json')
+const getCheckSuites = require('./__fixtures__/check-suites.get.json')
 
 exports.handlers = [
   rest.get('*/check-suites', (req, res, ctx) => {
-    const pathParts = req.url.pathname.split('/')
-    const commitRef = pathParts[pathParts.length - 2]
-
-    // TODO: if commitRef === all-checks-passed | only-checks-complete | only-checks-passed | no-checks-passed
-
-    return res(ctx.json(allChecksPassed))
+    return res(ctx.json(getCheckSuites))
   }),
   rest.get('*', (req, res, ctx) => {
     console.log('*', req.method, req.url)
