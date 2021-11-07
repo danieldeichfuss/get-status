64df4e95e829ee649cb5dea78ec6ef7c0a6ad95c
danieldeichfuss <d.deichfuss@gmail.com>
2021-11-07 01:10:35 +0100

    Log check suites

--- dist/index.js
+++ dist/index.js
@@ -149,6 +149,25 @@ __exportStar(__nccwpck_require__(4444), exports);
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -160,11 +179,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getStatus = void 0;
+const core = __importStar(__nccwpck_require__(2186));
 const fetch_checks_1 = __nccwpck_require__(4476);
 function getStatus({ ref, token }) {
     return __awaiter(this, void 0, void 0, function* () {
         const checks = yield (0, fetch_checks_1.fetchChecks)({ ref, token });
         const checkSuites = checks === null || checks === void 0 ? void 0 : checks.check_suites;
+        core.info(`Check Suites: ${JSON.stringify(checks)}`);
         const allChecksCompleted = checkSuites === null || checkSuites === void 0 ? void 0 : checkSuites.every(checkSuite => {
             return checkSuite.status === 'completed';
         });
